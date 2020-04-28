a761ef02ea8baca1e9a47f2ea504808fff6e5aaa
Laith Raed <laith@gmx.de>
2020-04-28 14:35:22 +0200

    CustomerRest findAllCustomer()

--- src/main/java/com/bmw/rest/CustomerRest.java
+++ src/main/java/com/bmw/rest/CustomerRest.java
@@ -1,18 +1,28 @@
 package com.bmw.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.bmw.entity.Customer;
 import com.bmw.service.CustomerService;
+import com.bmw.service.QueryService;
 
 @Path("customer")  //url/car2/resources/customer
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class CustomerRest {
 
 	@Inject
 	CustomerService customerService; 
+	
+	@Inject
+	QueryService queryService;
 	
 	
 	@Path("create")  // url/car2/customer/create
@@ -22,5 +32,10 @@ public class CustomerRest {
 		return Response.ok(customer).build();
 	}
 	
+	@Path("list") // url/car2/customer/list
+	@GET
+	public Response findAllCustomers() {
+		return Response.ok(queryService.findAllCustomers()).build();
+	}
 	
 }
