1b73d5d047ab642ede8baf38a417f0de036e488b
Laith Raed <laith@gmx.de>
2020-04-28 13:45:22 +0200

    CustomerService mit create

--- src/main/java/com/bmw/service/CustomerService.java
+++ src/main/java/com/bmw/service/CustomerService.java
@@ -1,5 +1,22 @@
 package com.bmw.service;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.bmw.entity.Customer;
+
+@Stateless
 public class CustomerService {
 
+	@PersistenceContext   
+	EntityManager entityManager;
+	
+	
+	// Create
+		public Customer create(Customer customer) {
+			entityManager.persist(customer);
+			return customer;
+		}
+	
 }
