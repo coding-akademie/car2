395a00f3b6ebd80a7e7a1cad9cabbf235da55871
Laith Raed <laith@gmx.de>
2020-04-28 13:29:48 +0200

    Customer class

---
+++ src/main/java/com/bmw/entity/Customer.java
@@ -0,0 +1,59 @@
+package com.bmw.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String name; // NAME
+
+	// @NotEmpty(message = "Email darf nicht leer sein!")
+	private String email; // EMAIL
+
+	private LocalDate createdDate;
+	private boolean active;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public LocalDate getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(LocalDate createdDate) {
+		this.createdDate = createdDate;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	
+	
+	
+}
