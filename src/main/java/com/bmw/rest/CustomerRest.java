6594d6e001ce83bcdc2a6a5d1063533b8efadd0d
Laith Raed <laith@gmx.de>
2020-04-28 13:59:04 +0200

    Persistence XML

--- src/main/java/com/bmw/rest/CustomerRest.java
+++ src/main/java/com/bmw/rest/CustomerRest.java
@@ -1,5 +1,26 @@
 package com.bmw.rest;
 
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import com.bmw.entity.Customer;
+import com.bmw.service.CustomerService;
+
+@Path("customer")  //url/car2/resources/customer
 public class CustomerRest {
 
+	@Inject
+	CustomerService customerService; 
+	
+	
+	@Path("create")  // url/car2/customer/create
+	@POST
+	public Response create(Customer customer) {
+		customerService.create(customer);
+		return Response.ok(customer).build();
+	}
+	
+	
 }
